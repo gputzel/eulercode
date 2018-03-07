57f5045429f6db6398b99dcfdb15d26a08a9d257
Gregory Putzel <grp2009@med.cornell.edu>
2018-03-07 17:03:00 -0500

    Calculate Fibonacci numbers using matrix multiplication

--- fibonacci-matrix.hs
+++ fibonacci-matrix.hs
@@ -1,5 +1,21 @@
-newtype TBT a = TBT ((a,a),(a,a)) deriving (Eq,Show)
+newtype TBT = TBT ((Integer,Integer),(Integer,Integer)) deriving (Eq,Show)
 
-m :: TBT Integer
+m :: TBT
 m = TBT ((1,1),(1,0))
 
+instance Num TBT where
+    fromInteger n = TBT ((n,0),(0,n))
+    TBT ((a11,a12),(a21,a22)) * TBT ((b11,b12),(b21,b22)) = TBT ((c11,c12),(c21,c22))
+        where
+            c11 = a11*b11 + a12*b21
+            c12 = a11*b12 + a12*b22
+            c21 = a21*b11 + a22*b21
+            c22 = a21*b12 + a22*b22
+
+upperLeft :: TBT -> Integer
+upperLeft (TBT ((a11,a12),(a21,a22))) = a11
+
+--Implementation with naive exponentiation
+--linear time
+fibSlow :: Integer -> Integer
+fibSlow n = upperLeft $ m ^(n-1)
