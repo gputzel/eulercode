47add2629012e05f290b501fc353a896d2538113
Gregory Putzel <grp2009@med.cornell.edu>
2018-03-07 16:33:38 -0500

    Reimplement S5N as a type with a numeric type parameter

--- problem2-binet.hs
+++ problem2-binet.hs
@@ -1,56 +1,51 @@
+import Data.Ratio
+
 -- (S5N) a b represents a + b*sqrt(5)
-data S5N = S5N Integer Integer deriving Show
+newtype S5N a = S5N (a,a) deriving (Eq,Show)
+
+instance (Num a) => Num (S5N a) where
+    S5N (a,b) + S5N (c,d) = S5N (a+c,b+d)
+    S5N (a,b) - S5N (c,d) = S5N (a-c,b-d)
+    S5N (a,b) * S5N (c,d) = S5N (a*c + 5*b*d, a*d + b*c)
+    fromInteger i = S5N (fromInteger i,0)
 
 --By analogy with complex numbers, the "imaginary" part
---of a + b*sqrt(5) is b
-ipart :: S5N -> Integer
-ipart (S5N a b) = b
+--of m + n*sqrt(5) is b
+ipart :: (S5N a) -> a
+ipart (S5N (m,n)) = n
 
 --By analogy with complex numbers, the "real" part 
---of a + b*sqrt(5) is a
-rpart :: S5N -> Integer
-rpart (S5N a b) = a
-
---Define operations of multiplication and subtraction
---(a + b*sqrt(5)) * (c + d*sqrt(5))
-mult :: S5N -> S5N -> S5N
-(S5N a b) `mult` (S5N c d) = S5N (a*c + 5*b*d) (a*d + b*c)
-
-plus :: S5N -> S5N -> S5N
-(S5N a b) `plus` (S5N c d) = S5N (a + c) (b + d)
-
-minus :: S5N -> S5N -> S5N
-(S5N a b) `minus` (S5N c d) = S5N (a - c) (b - d)
+--of m + n*sqrt(5) is a
+rpart :: (S5N a) -> a
+rpart (S5N (m,n)) = m
 
 --Naive implementation of exponentiation
 --Linear in n
-powSlow :: S5N -> Integer -> S5N
-powSlow _ 0 = (S5N 1 0)
-powSlow s n = s `mult` (powSlow s (n-1))
+powSlow :: Num a => (S5N a) -> Integer -> (S5N a)
+powSlow _ 0 = fromInteger 1
+powSlow s n = s * (powSlow s (n-1))
 
 --Faster implementation of exponentiation
 --Relies on binary expansion of n
 --Logarithmic in n
-pow :: S5N -> Integer -> S5N
-pow _ 0 = (S5N 1 0)
+pow :: Num a => (S5N a) -> Integer -> (S5N a)
+pow _ 0 = fromInteger 1
 pow s n
-    | odd n = s `mult` (root `mult` root)
-    | otherwise = (root `mult` root)
+    | odd n = s * (root * root)
+    | otherwise = (root * root)
         where root = (s `pow` (n `div` 2))
 
---Well, actually 2*phi and 2*psi
---We will divide by the power of 2 at the end
-phi = S5N 1 1
-psi = S5N 1 (-1)
+phi = S5N (1 % 2, 1 % 2)
+psi = S5N (1 % 2, (-1) % 2)
 
 --Find the nth Fibonacci number usng the Binet formula
 --Uses slow (linear time) exponentiation
 fibSlow :: Integer -> Integer
-fibSlow n = (ipart difference) `div` (2^n)
-            where difference = (phi `powSlow` n) `minus` (psi `powSlow` n)
+fibSlow n = numerator (ipart difference)
+            where difference = (phi `powSlow` n) - (psi `powSlow` n)
 
 --Find the nth Fibonacci number usng the Binet formula
 --Uses faster (logarithmic time) exponentation
 fib :: Integer -> Integer
-fib n = (ipart difference) `div` (2^n)
-            where difference = (phi `pow` n) `minus` (psi `pow` n)
+fib n = numerator (ipart difference)
+            where difference = (phi `pow` n) - (psi `pow` n)
